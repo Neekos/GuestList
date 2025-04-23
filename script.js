0a99a49487473f7f7e4471bf6157b3bfccbee77b
Андрей Спиридонов <30973902+Neekos@users.noreply.github.com>
2025-04-23 22:27:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-alert(123)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAt3VdtvCexC5DcuvDv7ZXc-D1XoWXh0qM",
-    authDomain: "guestlist-345f8.firebaseapp.com",
-    projectId: "guestlist-345f8",
-    storageBucket: "guestlist-345f8.firebasestorage.app",
-    messagingSenderId: "195999212818",
-    appId: "1:195999212818:web:11f8d4a149b7a2cb4d7fcb",
-    measurementId: "G-2MPWYBQ8LR"
+  apiKey: "AIzaSyAt3VdtvCexC5DcuuDv7ZXc-D1XoWXh0qM",
+  authDomain: "guestlist-345f8.firebaseapp.com",
+  projectId: "guestlist-345f8",
+  storageBucket: "guestlist-345f8.firebasestorage.app",
+  messagingSenderId: "195999212818",
+  appId: "1:195999212818:web:11f8d4a149b7a2cb4d7fcb",
+  measurementId: "G-2MPWYBQ8LR"
 };
 firebase.initializeApp(firebaseConfig);
 
