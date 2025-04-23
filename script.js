68dc728e673cbfb8db61c1ac238412910a250ce4
Андрей Спиридонов <30973902+Neekos@users.noreply.github.com>
2025-04-23 23:07:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAt3VdtvCexC5DcuuDv7ZXc-D1XoWXh0qM",
+  authDomain: "guestlist-345f8.firebaseapp.com",
+  databaseURL: "https://guestlist-345f8-default-rtdb.firebaseio.com",
+  projectId: "guestlist-345f8",
+  storageBucket: "guestlist-345f8.firebasestorage.app",
+  messagingSenderId: "195999212818",
+  appId: "1:195999212818:web:11f8d4a149b7a2cb4d7fcb",
+  measurementId: "G-2MPWYBQ8LR"
+};
+firebase.initializeApp(firebaseConfig);
 document.getElementById('saveButton').addEventListener('click', function () {
     // Получение данных из формы
     var name = document.getElementById('name').value;
